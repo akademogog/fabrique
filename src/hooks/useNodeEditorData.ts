19df23784713263b1003af46ceb330cceb64dbd8
artavazd_b <bahshyanmainacc@gmail.com>
2023-01-24 17:13:32 +0300

    change node editor

--- src/hooks/useNodeEditorData.ts
+++ src/hooks/useNodeEditorData.ts
@@ -1,38 +1,35 @@
 import { objectToArray } from "@/helpers/mapping";
 import { useAppSelector } from "@/store/hooks";
-import { Node } from "@/types/node.types";
-import { useEffect, useMemo, useState } from "react";
+import { NodeData } from "@/types/node.types";
+import { useEffect, useMemo, useState } from "react"
+import { Node } from "reactflow";
 
-export const useNodeEditorData = (
-  area: "actor" | "pipeline",
-  areaID: any,
-  nodeID: any
-) => {
-  const actor = useAppSelector((state) => {
-    const currentState = objectToArray(
-      state[area === "pipeline" ? "pipelines" : "actors"]
-    );
-    return currentState.find((el) => el.id === areaID);
-  });
-  const selectedNode: Node | undefined = useMemo(() => {
-    if (actor) {
-      const nodes = objectToArray(actor.nodes);
-      return nodes.find((node) => node.id === nodeID);
-    }
-  }, [actor]);
 
-  const inputs: any = useMemo(() => {
-    if (selectedNode) {
-      const inputs = objectToArray(selectedNode.data.inputs);
-      return inputs;
-    }
-  }, [selectedNode]);
-  const outputs: any = useMemo(() => {
-    if (selectedNode) {
-      const outputs = objectToArray(selectedNode.data.outputs);
-      return outputs;
-    }
-  }, [selectedNode]);
+export const useNodeEditorData = (area: 'actor' | 'pipeline', areaID: any, nodeID: any) => {
+    const actor = useAppSelector((state) => {
+        const currentState = objectToArray(state[area === 'pipeline'? 'pipelines' : 'actors']);
+        return currentState.find(el => el.id === areaID);
+      });
+      const selectedNode: Node<NodeData> = useMemo(() => {
+        if (actor) {
+          const nodes = objectToArray(actor.nodes);
+          return nodes.find((node) => node.id === nodeID);
+        }
+      }, [actor]);
+    
+      const inputs: any = useMemo(() => {
+        if (selectedNode) {
+          const inputs = objectToArray(selectedNode.data.g_ports_in);
+          return inputs;
+        }
+      }, [selectedNode]);
+      const outputs: any = useMemo(() => {
+        if (selectedNode) {
+          const outputs = objectToArray(selectedNode.data.g_ports_out);
+          return outputs;
+        }
+      }, [selectedNode]);
 
-  return { actor, selectedNode, inputs, outputs };
-};
+      return {actor, selectedNode, inputs, outputs}
+
+}
--- src/store/slicers/pipelinesSlicer.ts
+++ src/store/slicers/pipelinesSlicer.ts
@@ -110,7 +110,7 @@ export const pipelinesSlice = createSlice({
       const payload = action.payload;
       state[payload.pipelineID].nodes[payload.nodeID].data[payload.type][payload.inputID] = {
         ...state[payload.pipelineID].nodes[payload.nodeID].data[payload.type][payload.inputID],
-        value: payload.value
+        code: payload.value
       }
     },
     appendPipelineNodeInput: (
